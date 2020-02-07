10f6c484e28b7c2ab70918046c491a151df79bb5
tara <59029999+taralovestea@users.noreply.github.com>
2020-02-07 14:08:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,52 @@
 var APIKey = "0cd5d0e1fa1a89ffdba4e7569fe2ff57";
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
-   var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=zip_value_replace,us&appid=" + APIKey;
+var userCity= "";
 
-console.log(APIKey)
 
-function call_weather_ap(a_zipcode){
-  console.log("a_zipcode:")
-  console.log(a_zipcode)
-  new_q = queryURL.replace("zip_value_replace", a_zipcode)
-  console.log(new_q)
+$("#searchbar_form").submit(function(event){
+  event.preventDefault();
+  userCity= $("#zipcode_id").val()
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +userCity + "&appid=" + APIKey;
   $.ajax({
-    url: new_q,
-    type: 'GET',
-    dataType: 'json', // added data type
-    success: function(res) {
-      // console.log(res);
-      calculations(res);
-      // console.log(res);
-      // console.log(res.main);
-      // console.log(res.main.temp);
-
-      console.log(res);}
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    console.log(queryURL);
+    let results = [response.name, response.wind.speed, response.wind.deg, response.main.humidity, response.main.temp,]
+    console.log(results);
+    $(".city").append("<h2>In " + (response.name) + ", " + (response.sys.country)+ "<img src=http://openweathermap.org/img/wn/"+(response.weather[0].icon)+".png></h2>");
+    $(".temp").append("the current temperature is " + Math.floor((response.main.temp - 273.15) * 1.80 + 32) + " &deg;F");
+    $(".humidity").append("the humidity level is " + (response.main.humidity) + " %");
+    $(".windspeed").append("the wind speed is " + response.wind.speed + " MPH");
+  
+  var lat= response.coord.lat
+  var lon= response.coord.lon
+  var uVurl= "http://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+lat+"&lon="+lon;
+  var fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?id="+response.id+ "&appid=" +APIKey;
+  $.ajax({
+    url: uVurl,
+    method: "GET"
+  }).then(function(response) {
+  console.log(response)
+  $(".uv-index").append("the UV index is "+ response.value);
   });
-}
-
-// we get the parameter from the URL
-const queryString = window.location.search;
-console.log(queryString);
-const urlParams = new URLSearchParams(queryString);
-const zipcode_value = urlParams.get('zipcode')
-console.log(zipcode_value);
-searchbar_form.onsubmit = searchZipcode(zipcode_value);
-
-function searchZipcode(a_zipcode){
-   call_weather_ap(zipcode_value)
-}
-
-function calculations(a_res){
-  console.log(a_res)
-  temp = a_res.main.temp
-  console.log(temp)
-  temp_f = Math.floor((temp - 273.15) * 1.80 + 32)
-  console.log(temp_f)
-
-}
-
-/*
-.then(function(response) {
-  let results = [response.name, response.wind.speed, response.wind.deg, response.main.humidity, response.main.temp,]
-  console.log(results);
-
-  $(".city").append("In " + (response.name) + ", " + (response.sys.country) + "... ");
-  $(".temp").append("...the current temperature is " + Math.floor((response.main.temp - 273.15) * 1.80 + 32) + " &deg;F");
-  $(".humidity").append("...the humidity level is " + (response.main.humidity) + " %");
-  $(".windspeed").append("...the wind speed is " + response.wind.speed + " MPH");
-//   $(".uv-index").append(response.uvindex);
-});
-*/
+  $.ajax({
+    url: fiveDayUrl,
+    method: "GET"
+  }).then(function(response) {
+  console.log(response)
+  var addedDays = 1;
+  for (var i=0; i<40; i += 9) {
+    console.log(response.list[i])
+    var dayNow = moment().add(addedDays, 'days').format('MM/DD/YYYY')
+    addedDays++
+    var fiveDayBlock = $(`<div class= "forecast">
+                          <h5> ${dayNow} </h5>
+                          <img src=http://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png>
+                          <p> ${Math.floor(((response.list[i].main.temp - 273.15) * 1.8) + 32)} Degrees </p>
+                          <p> ${response.list[i].main.humidity} % Humidity </p>`)
+    $("#forecast").append(fiveDayBlock);
+  }
+  });
+  });
+})
